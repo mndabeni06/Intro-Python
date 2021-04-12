b48b7041ecf74c59101d500e9042904551ffd432
Masimthembe <mndabeni6@gmail.com>
2021-04-12 16:34:04 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,8 @@
+print("Hello World")
+_PI = 3.14
+radius = int (input("Please enter the radius: "))
+area = _PI * radius**2
+print("The area of a circle is: " + str(area))
+
+diameter = 2 * radius
+print("The diameter of a circle is: " + str (diameter))
